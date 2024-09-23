e9c6a13776c197e9f0c943abe3a1aa9814e88d78
AnshulKaryal <karyalanshul@gmail.com>
2024-09-23 17:31:39 +0530

    cors

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const product_routes = require("./routes/products");
 const app = express();
 
 app.use(cors({
-	origin: CorsConfig,
+	origin: '*',
 	credentials: true,
 })); // Enable CORS for all routes
 
